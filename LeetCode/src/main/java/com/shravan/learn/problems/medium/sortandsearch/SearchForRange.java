15f8ca789cf730541af479e9ef334ced33ba3454
shravan.shan@gmail.com <5rashvan888>
2023-05-18 20:46:17 +0530

    bisect left right fns added

--- LeetCode/src/main/java/com/shravan/learn/problems/medium/sortandsearch/SearchForRange.java
+++ LeetCode/src/main/java/com/shravan/learn/problems/medium/sortandsearch/SearchForRange.java
@@ -47,4 +47,28 @@ public class SearchForRange {
         // End Condition: left > right
         return -1;
     }
+
+    private int bisectRight(int[] A, int right, int target) {
+        int left = 0;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (A[mid] <= target)
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        return left;
+    }
+
+    private int bisectLeft(int[] A, int right, int target) {
+        int left = 0;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (A[mid] < target)
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        return left;
+    }
 }
